738fae33f489e6c0bb5ed398a81a62ea7216aa4f
toryzen <toryzen@hotmail.com>
2018-03-30 20:04:07 +0800

    fix

--- control.cmd
+++ control.cmd
@@ -13,20 +13,22 @@ if %errorlevel%==0 ( rd "%SystemRoot%\system32\%uac%" >nul 2>nul ) else (
 endlocal  
 
 :BG
-echo ©°©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©´
-echo ©¦                               SmartPing                              ©¦
-echo ©À©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©È
-echo ©¦                                                                      ©¦
-echo ©¦Instruction USE                                                       ©¦
-echo ©¦        build   run go get and build                                  ©¦
-echo ©¦        run     run smartping                                         ©¦
-echo ©¦        install install smartping as service (use nssm)               ©¦
-echo ©¦        start   start smartping service                               ©¦
-echo ©¦        stop    stop smartping service                                ©¦
-echo ©¦        restart stop and start smartping                              ©¦
-echo ©¦        version show smartping version                                ©¦
-echo ©¦                                                                      ©¦
-echo ©¸©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¼
+cls
+echo ©°©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©´
+echo ©¦                        SmartPing                            ©¦
+echo ©À©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©È
+echo ©¦                                                             ©¦
+echo ©¦INS USE                                                      ©¦
+echo ©¦        build      run go get and build                      ©¦
+echo ©¦        run        run smartping                             ©¦
+echo ©¦        install    install smartping as service (use nssm)   ©¦
+echo ©¦        uninstall  uninstall smartping service               ©¦
+echo ©¦        start      start smartping service (after install)   ©¦
+echo ©¦        stop       stop smartping service                    ©¦
+echo ©¦        restart    stop and start smartping                  ©¦
+echo ©¦        version    show smartping version                    ©¦
+echo ©¦                                                             ©¦
+echo ©¸©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¼
 
 %~d0
 cd %~dp0
@@ -37,28 +39,42 @@ IF "%select%"=="build" (
     ::go build -o %~dp0\bin\goping.exe  %~dp0\src\ping\goping\goping.go
     go build -o %~dp0\bin\smartping.exe  %~dp0\src\smartping.go
     echo Build Finish.. 
+    pause
+    GOTO BG
 ) ELSE (
     IF "%select%"=="run" (
         %~dp0/bin/smartping.exe 
     ) ELSE ( 
         IF "%select%"=="install" (
             %~dp0\\bin\\nssm.exe install smartping %~dp0\\bin\\smartping.exe 
+            pause
+            GOTO BG
         ) ELSE ( 
             IF "%select%"=="start" (
                 net start smartping 
+                pause
+                GOTO BG
             ) ELSE (
                 IF "%select%"=="stop" (
                     net stop smartping 
+                    pause
+                    GOTO BG
                 ) ELSE (
                     IF "%select%"=="restart" (
                         net stop smartping 
                         net start smartping 
+                        pause
+                        GOTO BG
                     ) ELSE (
                         IF "%select%"=="uninstall" (
                             sc delete smartping 
+                            pause
+                            GOTO BG
                         ) ELSE (
                              IF "%select%"=="version" (
                                 %~dp0\bin\smartping.exe -v 
+                                pause
+                                GOTO BG
                             ) ELSE (
                                  echo Param Error Try Again!
                                  pause
